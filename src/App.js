6d8352e2c6216bf9abb6dc438fd2c1cc96f1d4ca
p.balmasov <p.balmasov@gmail.com>
2023-11-29 12:31:59 +0300

    concentration inputs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
 // import logo from './logo.svg';
 import './App.css';
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
+
+const refColors = [
+    {r: 203, g: 191, b: 165, a: 1, coff: 5},
+    {r: 185, g: 165, b: 135, a: 1, coff: 3},
+    {r: 166, g: 139, b: 105, a: 1, coff: 1},
+    {r: 148, g: 113, b: 74, a: 1, coff: 0.5},
+    {r: 129, g: 87, b: 44, a: 1, coff: 0},
+].map(({b, g, r, coff}) => ({data: [r, g, b, 255], coff}));
+
+function toRGBAString({data}) {
+    return `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
+}
+
+function toRGBA({data}) {
+    return {r: data[0], g: data[1], b: data[2], a: data[3]};
+}
+
+function getDiffColor(a, b) {
+    return Math.sqrt(Math.pow((a.r - b.r), 2) + Math.pow((a.g - b.g), 2) + Math.pow((a.b - b.b), 2));
+}
+
+function updateActiveColors(colors, col) {
+    colors.forEach(c => {
+        c.active = false;
+        c.diff = getDiffColor(toRGBA(col), toRGBA(c));
+    });
+    if (col) {
+        const diffs = colors.map(c => getDiffColor(toRGBA(col), toRGBA(c)));
+        const min1 = Math.min(...diffs);
+        const min2 = Math.min(...diffs.filter(item => item !== min1));
+        colors[diffs.indexOf(min1)].active = true;
+        colors[diffs.indexOf(min2)].active = true;
+    }
+}
 
 function App() {
-    const refColors = [
-        {
-            r: 129, g: 85, b: 50
-        },
-        {
-            r: 130, g: 90, b: 40
-        },
-        {
-            r: 136, g: 95, b: 51
-        },
-        {
-            r: 140, g: 100, b: 40
-        },
-        {
-            r: 120, g: 96, b: 40
-        }
-    ].map(({b, g, r}) => ({data:[r, g, b, 255]}));
-    const [colors, setColors] = useState([]);
+    const [color, setColor] = useState(null);
+    const [colors, setColors] = useState(() => {
+        const initialValue = JSON.parse(localStorage.getItem("colors"));
+        return initialValue || refColors;
+    });
     const fileInput = useRef();
+    useEffect(() => {
+        localStorage.setItem('colors', JSON.stringify(colors));
+    }, [colors]);
 
     function onImageChange(event) {
         const {files} = event.target;
@@ -34,16 +58,16 @@ function App() {
                 context.drawImage(img, 0, 0, 1, 1);
                 const average = context.getImageData(0, 0, 1, 1);
 
-                setColors([average]);
+                setColor(average);
+                const state = [...colors];
+                updateActiveColors(state, average);
+                setColors(state);
                 context.drawImage(img, 100, 100);
             }
             img.src = URL.createObjectURL(files[0]);
         }
     }
 
-    function toRGBA({data}) {
-        return `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
-    }
 
     function pick(event) {
         const context = canvas.current.getContext("2d");
@@ -53,7 +77,7 @@ function App() {
         const y = event.clientY - bounding.top;
         const pixel = context.getImageData(x, y, 1, 1);
 
-        setColors([pixel]);
+        setColor(pixel);
     }
 
     /*    function average(context){
@@ -76,69 +100,118 @@ function App() {
 
     const canvas = useRef();
 
-    function drawColors(colors) {
+    function drawColors(colors, isInput) {
         return (<ul style={{display: 'flex'}}>
             {colors.map((color, index) =>
                 <li key={index} style={{margin: 15, listStyle: 'none'}}>
-                    <div style={{
-                        backgroundColor: toRGBA(color),
-                        display: 'block',
-                        borderRadius: '50%',
-                        width: 50,
-                        height: 50
-                    }}/></li>
+                    <label>
+                        <div style={{
+                            backgroundColor: toRGBAString(color),
+                            display: 'block',
+                            borderRadius: '50%',
+                            width: 50,
+                            height: 50,
+                            border: color.active ?'solid 4px green':'solid 4px transparent'
+                        }}/>
+                        <input type='file' onChange={(e) => {
+                            const {files} = e.target;
+                            if (files && files[0]) {
+                                const img = new Image();
+                                img.onload = function () {
+                                    const context = canvas.current.getContext("2d");
+
+                                    context.imageSmoothingEnabled = true;
+                                    context.drawImage(img, 0, 0, 1, 1);
+                                    const state = [...colors];
+                                    state[index].data = context.getImageData(0, 0, 1, 1).data;
+                                    updateActiveColors(state, color);
+                                    setColors(state);
+                                }
+                                img.src = URL.createObjectURL(files[0]);
+                            }
+                        }
+                        }
+                               accept="image/png, image/jpeg"
+                               style={{display: 'none'}}
+                        />
+                    </label>
+                    {isInput && <input
+                        value={color.coff}
+                        type="number" step="0.01"
+                        style={{width: 50}}
+                        onChange={e => {
+                            const state = [...colors];
+                            state[index].coff = e.target.value;
+                            updateActiveColors(state, color);
+                            setColors(state);
+                        }}
+                    />}
+                </li>
             )}
         </ul>)
     }
 
-    function getDiffColor(a,b) {
-        return Math.sqrt(Math.pow((a.r - b.r),2) + Math.pow((a.g - b.g),2) + Math.pow((a.b - b.b),2));
+    /*function normalize(min, max) {
+        var delta = max - min;
+        return function (val) {
+            return (val - min) / delta;
+        };
+    }*/
+
+    let concentration;
+
+    const isActive = colors.filter(item => item.active);
+    if (isActive.length !== 0) {
+        const diffs = colors.map(c => c.diff);
+        const min1 = Math.min(...diffs);
+        const min2 = Math.min(...diffs.filter(item => item !== min1));
+        const diff = min2 + min1;
+        concentration = colors[diffs.indexOf(min1)].coff * (min2 / diff) + colors[diffs.indexOf(min2    )].coff * (1 - min2 / diff);
     }
 
-    // let closest;
-    if(colors[0]){
-        const diffs = refColors.map(color=>getDiffColor(colors[0],color));
-        console.log(diffs.indexMath.min(...diffs));
-    }
+    /*if (colors[0]) {
+        const diffs = refColors.map(color => getDiffColor(toRGBA(colors[0]), toRGBA(color)));
+        const min1 = Math.min(...diffs);
+        const min2 = Math.min(...diffs.filter(item => item !== min1));
+        // console.log(min1, min2);
+        const diff = min2 + min1;
+        // console.log(min2 / diff,);
+        concentration = refColors[diffs.indexOf(min1)].coff * (min2 / diff) + refColors[diffs.indexOf(min2)].coff * (1 - min2 / diff);
+        // console.log('normalize:', diffs.map(normalize(min, max)))
+        // concentration = diffs.indexOf(min1);
+    }*/
 
     return (
         <div className="App">
-            {/*<header className="App-header">*/}
-            {/*<img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>*/}
-            {/*<form onSubmit={handleSubmit}>*/}
-            <input type='file' ref={fileInput} onChange={onImageChange}/>
-            {/*{image && <img alt="preview image" src={image}/>}*/}
-            <canvas ref={canvas} width={500} height={500} onPointerDown={(e) => pick(e)}/>
-            {/*</form>*/}
-            {/*</header>*/}
+            <input type='file' ref={fileInput} onChange={onImageChange}
+                   accept="image/png, image/jpeg"
+            />
+            <canvas style={{
+                display: 'block',
+                margin: '0 auto',
+                width: '100%',
+                maxWidth: '500px',
+            }} ref={canvas} width={500} height={500} onPointerDown={(e) => pick(e)}/>
 
-            <div style={{
+            {color && <div style={{
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center'
             }}>
-                Picked
-                {drawColors(colors)}
+                Picked:
+                {drawColors([color], false)}
             </div>
+            }
             <div style={{
                 display: 'flex',
                 justifyContent: 'center',
-                alignItems: 'center'
+                alignItems: 'center',
+                flexWrap: 'wrap'
             }}>
-                Reference
-                {drawColors(refColors)}
+                Reference:
+                {drawColors(colors, true)}
             </div>
+            {concentration && <div>Concentration: {concentration.toFixed(2)}</div>}
         </div>
     );
 }
